e86c3e37adbcb2920c0348ed9437d9b9be30c4ab
Flyer <giogaruffi@gmail.com>
2015-03-17 17:50:39 -0300

    Finalizado

--- src/pt/c02classes/s01knowledge/s02app/actors/EnquirerAnimals.java
+++ src/pt/c02classes/s01knowledge/s02app/actors/EnquirerAnimals.java
@@ -1,6 +1,8 @@
 package pt.c02classes.s01knowledge.s02app.actors;
 
+//Importa as bibliotecas de ArrayList e Vector
 import java.util.ArrayList;
+import java.util.Vector;
 import pt.c02classes.s01knowledge.s01base.impl.BaseConhecimento;
 import pt.c02classes.s01knowledge.s01base.inter.IBaseConhecimento;
 import pt.c02classes.s01knowledge.s01base.inter.IDeclaracao;
@@ -17,28 +19,38 @@ public class EnquirerAnimals implements IEnquirer {
 	}
 	
 	public boolean discover() {
+		//Cria a base de conhecimento e coloca o cenario como "animals"
         IBaseConhecimento bc = new BaseConhecimento();
+        bc.setScenario("animals");
     	IObjetoConhecimento obj;
 
-        boolean acertei = true;
+        boolean acertei = false;
 		
+        //Cria ArrayLists para armazenar as perguntas realizadas e suas respostas
 		ArrayList<String> perguntadas = new ArrayList<String>();
 		ArrayList<String> respondidas = new ArrayList<String>();
-
+		//Cria Vector para armazenar todos os possiveis animais
+		Vector<String> listaAnimais = bc.listaNomes();
+		
 		boolean animalEsperado = false;
 
-		for(String escolhido : bc.listaNomes()){
-			
-			obj = bc.recuperaObjeto(escolhido);
+		String animal_teste;
+		//Enquanto nao sao testados todos os animais ou o correto nao eh encontrado
+		while(!listaAnimais.isEmpty() && !acertei){
+			//Pega o primeiro elemento do Vector e testa se eh o animal pensado
+			animal_teste = listaAnimais.firstElement();
+			obj = bc.recuperaObjeto(animal_teste);
+			listaAnimais.remove(0);
 			IDeclaracao decl = obj.primeira();
 			animalEsperado = true;
 
 			String resposta;
-
+			//Realiza as perguntas do animal a ser testado como o pensado
 			while (decl != null && animalEsperado) {
 				String pergunta = decl.getPropriedade();
 				String respostaEsperada = decl.getValor();
 
+				//Testa o caso de ser uma pergunta repetida
 				if (!perguntadas.contains(pergunta)){
 					perguntadas.add(pergunta);
 				
@@ -54,13 +66,19 @@ public class EnquirerAnimals implements IEnquirer {
 					animalEsperado = false;
 				}
 			}
+			//Verifica se o animal testado eh de fato o pensado
 			if (animalEsperado)
-				acertei = responder.finalAnswer(escolhido);
-			if (acertei)
-				System.out.println("Oba! Acertei!");
-			else
-				System.out.println("fuem! fuem! fuem!");
+				acertei = responder.finalAnswer(animal_teste);
 		}
+		//Imprime resultado final
+		if (acertei)
+			System.out.println("Oba! Acertei!");
+		else
+			System.out.println("fuem! fuem! fuem!");
+		
+		listaAnimais.clear();
+		perguntadas.clear();
+		respondidas.clear();
 		
 		return acertei;
 	}
--- src/pt/c02classes/s01knowledge/s02app/actors/EnquirerMaze.java
+++ src/pt/c02classes/s01knowledge/s02app/actors/EnquirerMaze.java
@@ -8,6 +8,7 @@ import pt.c02classes.s01knowledge.s01base.inter.IResponder;
 public class EnquirerMaze implements IEnquirer {
 
 	IResponder responder;
+	//Declara Stack para armazenar o movimento recem realizada na classe
 	Stack <String> movimento;
 	
 	public void connect(IResponder responder) {
@@ -16,15 +17,19 @@ public class EnquirerMaze implements IEnquirer {
 	
 	boolean buscaSaida (){
 		
+		//Caso base para sair da recursao = encontrar saida
 		if (responder.ask("aqui") == "saida") 
 			return true; 
 		
 		boolean achei = false;
 		
+		//Testa se eh possivel se mover para o norte
 		if(!responder.ask("norte").equalsIgnoreCase("parede") 
 				&& !responder.ask("norte").equalsIgnoreCase("entrada")
 				&& !responder.ask("norte").equalsIgnoreCase("mundo")){
+			//Testa se nao eh o primeiro movimento e verifica que o ultimo movimento nao foi para o sul
 			if(movimento.empty() || !movimento.peek().equalsIgnoreCase("sul")){
+				//Realiza o movimento e armazena na Stack como o ultimo movimento valido
 				responder.move("norte");
 				System.out.println(" Movendo para: Norte ");
 				movimento.push("norte");
@@ -34,10 +39,13 @@ public class EnquirerMaze implements IEnquirer {
 				movimento.pop();
 			}
 		}
+		//Testa se eh possivel se mover para o sul e se nao foi encontrada a saida em uma iteracao anterior
 		if(!responder.ask("sul").equalsIgnoreCase("parede") 
 				&& !responder.ask("sul").equalsIgnoreCase("entrada") 
 				&& !responder.ask("sul").equalsIgnoreCase("mundo") && !achei){
+			//Testa se nao eh o primeiro movimento e verifica que o ultimo movimento nao foi para o norte
 			if(movimento.empty() || !movimento.peek().equalsIgnoreCase("norte")){
+				//Realiza o movimento e armazena na Stack como o ultimo movimento valido
 				responder.move("sul");
 				System.out.println(" Movendo para: Sul ");
 				movimento.push("sul");
@@ -47,10 +55,13 @@ public class EnquirerMaze implements IEnquirer {
 				movimento.pop();
 			}
 		}
+		//Testa se eh possivel se mover para o leste e se nao foi encontrada a saida em uma iteracao anterior
 		if(!responder.ask("leste").equalsIgnoreCase("parede") 
 				&& !responder.ask("leste").equalsIgnoreCase("entrada") 
 				&& !responder.ask("leste").equalsIgnoreCase("mundo") && !achei){
+			//Testa se nao eh o primeiro movimento e verifica que o ultimo movimento nao foi para o oeste
 			if(movimento.empty() || !movimento.peek().equalsIgnoreCase("oeste")){
+				//Realiza o movimento e armazena na Stack como o ultimo movimento valido
 				responder.move("leste");
 				System.out.println(" Movendo para: Leste ");
 				movimento.push("leste");
@@ -60,10 +71,13 @@ public class EnquirerMaze implements IEnquirer {
 				movimento.pop();
 			}
 		}
+		//Testa se eh possivel se mover para o oeste e se nao foi encontrada a saida em uma iteracao anterior
 		if(!responder.ask("oeste").equalsIgnoreCase("parede") 
 				&& !responder.ask("oeste").equalsIgnoreCase("entrada") 
 				&& !responder.ask("oeste").equalsIgnoreCase("mundo") && !achei){
+			//Testa se nao eh o primeiro movimento e verifica que o ultimo movimento nao foi para o leste
 			if(movimento.empty() || !movimento.peek().equalsIgnoreCase("leste")){
+				//Realiza o movimento e armazena na Stack como o ultimo movimento valido
 				responder.move("oeste");
 				System.out.println(" Movendo para: Oeste ");
 				movimento.push("oeste");
@@ -80,7 +94,8 @@ public class EnquirerMaze implements IEnquirer {
 	public boolean discover() {
 
 		movimento = new Stack<String> ();
-			
+		
+		//Saida padrao
 		if (buscaSaida())
 			System.out.println("Voc� encontrou a saida!");
 		else
